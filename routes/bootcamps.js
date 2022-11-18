cebe246a4261c8070afb648ea4de28cb7af28558
Adam Herro <adam.herro@comcast.net>
2022-11-18 16:11:49 -0500

    Adding database seeder for bootcamps, as well as adding geospatial queries, selecting, sorting, and pagination.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const { getBootcamps, getSingleBootcamp, createBootcamp, updateBootcamp, deleteBootcamp } = require('../controllers/bootcamps')
+const { getBootcamps, getSingleBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps')
 
 
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getSingleBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
