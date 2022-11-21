382f034dec65b35d98827ee50c49e0e4d7c57874
Adam Herro <adam.herro@comcast.net>
2022-11-21 16:17:29 -0500

    Adding course model, routes, and controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -3,6 +3,12 @@ const router = express.Router()
 const { getBootcamps, getSingleBootcamp, createBootcamp, updateBootcamp, deleteBootcamp, getBootcampsInRadius } = require('../controllers/bootcamps')
 
 
+// Include other resource routers
+const courseRouter = require('./courses')
+
+// Re-route into other resource router 
+router.use('/:bootcampId/courses', courseRouter)
+
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius)
 router.route('/').get(getBootcamps).post(createBootcamp)
 router.route('/:id').get(getSingleBootcamp).put(updateBootcamp).delete(deleteBootcamp)
